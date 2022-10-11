883e6ab911de4ce59ff145e9956e9f6c1011bdf8
Maemreyo <ngonhuthanhtrung1409@gmail.com>
2022-10-11 11:46:22 +0700

    Fix some falsy testcases

--- challenges/334/tests.spec.ts
+++ challenges/334/tests.spec.ts
@@ -4,17 +4,17 @@ import { increasingTriplet } from './solution'
 describe('Increasing triplet subsequence', () => {
 
     test("Should return the truthy value", () => {
-        expect(increasingTriplet([20,100,10,12,5,13])).toBeFalsy();
-    })
-    test("Should return the truthy value", () => {
         expect(increasingTriplet([1,2,3,4,5])).toBeTruthy();
         expect(increasingTriplet([5,4,1,2,3,4,5])).toBeTruthy();
         expect(increasingTriplet([2,1,3,4,5])).toBeTruthy();
+        expect(increasingTriplet([1,9,3,2,5])).toBeTruthy();
+        expect(increasingTriplet([2,1,3,0,5])).toBeTruthy();
+        expect(increasingTriplet([5,4,1,0,3,1,5])).toBeTruthy();
     })
 
     test("Should return the falsy value", () => {
-        expect(increasingTriplet([1,9,3,2,5])).toBeFalsy();
-        expect(increasingTriplet([5,4,1,0,3,1,5])).toBeFalsy();
-        expect(increasingTriplet([2,1,3,0,5])).toBeFalsy();
+        expect(increasingTriplet([5,4,3])).toBeFalsy();
+        expect(increasingTriplet([9,4,3])).toBeFalsy();
+        expect(increasingTriplet([5,1,3])).toBeFalsy();
     })
 })
